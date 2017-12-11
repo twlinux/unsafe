ddb008ee877cc9c86c2a5317db62e3884f37b68a
Jennings Zhang <jenni_zh@protonmail.com>
2017-12-11 18:21:47 -0500

    Migrate from jennydaman/twlinux unsafe_server

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Unsafe Server
+
+![badge](https://img.shields.io/badge/security-trash-red.svg)
+
+This is a crappy blog with a critical vulnerability.
+
+### Install Dependencies
+
+```bash
+npm install
+```
+
+### Run server
+
+```bash
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,37 @@
+const st = require('st');
+const ip = require('ip');
+const http = require('http');
+const colors = require('colors');
+
+var port = process.env.PORT || 1337; //default HTTP port is 80
+
+var mount = st({
+    path: `${__dirname}/blog`,
+    url: '/',
+    index: 'index.html',
+    gzip: false,
+    passthrough: true
+});
+
+http.createServer(function (request, response) {
+    mount(request, response, function () {
+        response.writeHead(404);
+        response.end();
+    });
+
+    let date = new Date();
+    console.log(colors.dim(`[  ${date.getHours()}:${date.getMinutes()} ${date.getSeconds()} ]`)
+        + ` Request from: ${colors.bold(request.headers.host)} wants: ${colors.cyan(request.url)}`);
+}).listen(port);
+
+let url = (`http://${colors.bold(ip.address())}:${colors.cyan(port.toString())}/`);
+console.log(`Server running at ${colors.underline(url)}`);
+
+process.on('SIGINT', function () {
+    console.log(`${'SIGINT'.underline.red} recieved, process exiting.`);
+    process.exit();
+});
+process.on('SIGTERM', function () {
+    console.log(`${'SIGTERM'.underline.red} recieved, process exiting.`);
+    process.exit();
+});
